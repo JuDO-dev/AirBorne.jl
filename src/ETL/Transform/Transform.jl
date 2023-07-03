79515e3eb21a3509398818740c7f93f208897955
brunocastroibarburu94 <57188560+brunocastroibarburu94@users.noreply.github.com>
2023-07-03 17:49:50 +0100

    added layer to define formats of stored data that can be used by cache and the rest of the package

---
+++ src/ETL/Transform/Transform.jl
@@ -0,0 +1,39 @@
+"""
+    Transform is the module were standard transformation between data structure takes place.
+
+    Particular datasources should adhere to data structures defined in this module. In particular
+    strategies and markets may make use of data structures in this module in order to promote cross-compatibility 
+    between different strategies, markets and engines.
+    
+"""
+module Transform
+export getSchema, addSchema
+
+using Tables: Schema
+using Dates: Dates
+using Bijections: Bijection, inv
+
+OHLCV_V1_specs = [
+    :close;         Float64;;
+    :high;          Float64;;
+    :low;           Float64;;
+    :open;          Float64;;
+    :volume;        Int64;;
+    :date;          Dates.DateTime;;
+    :unix;          Int64;;
+    :exchangeName;  String;;
+    :timezone;      String;;
+    :currency;      String;;
+    :symbol;        String;;
+   ]
+
+# Better implementation can be achieved by using Schemata.jl (left for ETL V2)
+schemas = Bijection{String,Schema}()
+getSchema(s::Schema)=get(inv(schemas),s,"UNREGISTERED_SCHEMA")
+getSchema(s::String)=get(schemas,s,nothing)
+function addSchema(name::String,schema::Schema)
+        schemas[name]=schema
+end
+addSchema("OHLCV_V1",Schema(OHLCV_V1_specs[1,:],OHLCV_V1_specs[2,:]))
+
+end

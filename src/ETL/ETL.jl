58869128c837751b7ed1711e4a99c168efcfa3be
brunocastroibarburu94 <57188560+brunocastroibarburu94@users.noreply.github.com>
2023-07-13 22:32:30 +0100

    folder names in github does pick up changes in lower case to uppercase and viceversa making the compilation fail.

--- src/ETL/ETL.jl
+++ src/ETL/ETL.jl
@@ -3,8 +3,8 @@
     connections, cache management and data transformations as part of the data pipeline.
 """
 module ETL
-include("./transform/Transform.jl") # Definition of structures and common transformations are here. This module must be included first.
-include("./transform/AssetValuation.jl")
+include("./Transform/Transform.jl") # Definition of structures and common transformations are here. This module must be included first.
+include("./Transform/AssetValuation.jl")
 include("./sources/NASDAQ.jl")
 include("./sources/YFinance.jl")
 include("./Cache/Cache.jl")

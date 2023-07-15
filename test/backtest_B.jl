f3c96841378ae8e119c88ee5305b3f2553a165b3
brunocastroibarburu94 <57188560+brunocastroibarburu94@users.noreply.github.com>
2023-07-15 18:45:16 +0100

    reintroduced SMA tests

--- test/backtest_B.jl
+++ test/backtest_B.jl
@@ -21,52 +21,52 @@ using Logging
     ######################
     ###  ContextTypeB  ###
     ######################
-    # dummy_event = TimeEvent(DateTime(2018, 1, 1), "example")
-    # contextB = ContextTypeB(dummy_event) # Initialize ContextTypeB
+    dummy_event = TimeEvent(DateTime(2018, 1, 1), "example")
+    contextB = ContextTypeB(dummy_event) # Initialize ContextTypeB
 
-    # @test length(dummy_event) == 1 # Events are atomic, thus have length 1.
-    # @test typeof(contextB.portfolio) == Portfolio
-    # @test typeof(contextB.accounts) == Wallet
+    @test length(dummy_event) == 1 # Events are atomic, thus have length 1.
+    @test typeof(contextB.portfolio) == Portfolio
+    @test typeof(contextB.accounts) == Wallet
 
-    # # addSecurityToPortfolio!(context.B)
-    # contextB.accounts += Money(10^5, :USD)
-    # @test contextB.accounts[:USD] == 10^5
-    # journal_entry = Dict(
-    #     "exchangeName" => "NMS",
-    #     "ticker" => "AAPL",
-    #     "shares" => 100.0,
-    #     "price" => Money(78.5, :USD),
-    #     "amount" => 100.0 * Money(78.5, :USD),
-    #     "date" => dummy_event.date,
-    # )
-    # journal_entry["assetID"] = keyJE(journal_entry)
-    # addMoneyToAccount!(contextB.accounts, journal_entry)
-    # addSecurityToPortfolio!(contextB.portfolio, journal_entry)
-    # vector_portfolio = []
-    # addSecurityToPortfolio!(vector_portfolio, journal_entry)
-    # @test size(vector_portfolio) == (1,)
-    # @test contextB.accounts[:USD] == 92150.0
-    # @test contextB.portfolio[Symbol(journal_entry["assetID"])] == 100.0
+    # addSecurityToPortfolio!(context.B)
+    contextB.accounts += Money(10^5, :USD)
+    @test contextB.accounts[:USD] == 10^5
+    journal_entry = Dict(
+        "exchangeName" => "NMS",
+        "ticker" => "AAPL",
+        "shares" => 100.0,
+        "price" => Money(78.5, :USD),
+        "amount" => 100.0 * Money(78.5, :USD),
+        "date" => dummy_event.date,
+    )
+    journal_entry["assetID"] = keyJE(journal_entry)
+    addMoneyToAccount!(contextB.accounts, journal_entry)
+    addSecurityToPortfolio!(contextB.portfolio, journal_entry)
+    vector_portfolio = []
+    addSecurityToPortfolio!(vector_portfolio, journal_entry)
+    @test size(vector_portfolio) == (1,)
+    @test contextB.accounts[:USD] == 92150.0
+    @test contextB.portfolio[Symbol(journal_entry["assetID"])] == 100.0
 
     cache_dir = joinpath(@__DIR__, "assets", "cache")
     data = load_bundle("demo"; cache_dir=cache_dir)
     ######################
     ###  SMA Strategy  ###
     ######################
-    # simulate_until = DateTime(2019, 2, 1)
-    # sma_initialize!(context) = interday_initialize!(context; longHorizon=20, shortHorizon=5)
-    # sma_trading_logic! = interday_trading_logic!
-    # context = run(
-    #     data,
-    #     sma_initialize!,
-    #     sma_trading_logic!,
-    #     execute_orders!,
-    #     expose_data;
-    #     audit=true,
-    #     max_date=DateTime(2019, 2, 1),
-    # )
-    # @test size(context.audit.portfolioHistory) == (741,)
-    # @test size(summarizePerformance(data, context; removeWeekend=true), 1) == 531
+    simulate_until = DateTime(2019, 2, 1)
+    sma_initialize!(context) = interday_initialize!(context; longHorizon=20, shortHorizon=5)
+    sma_trading_logic! = interday_trading_logic!
+    context = run(
+        data,
+        sma_initialize!,
+        sma_trading_logic!,
+        execute_orders!,
+        expose_data;
+        audit=true,
+        max_date=DateTime(2019, 2, 1),
+    )
+    @test size(context.audit.portfolioHistory) == (741,)
+    @test size(summarizePerformance(data, context; removeWeekend=true), 1) == 531
 
     ############################
     ###  Markowitz Strategy  ###
